2910a8fd7432269bb68f7fa52b1c67cc64b9c39f
Chris Duck <gpduck@whatsupduck.net>
2021-12-30 03:58:27 -0600

    allow filtered builds

--- build.ps1
+++ build.ps1
@@ -1,9 +1,11 @@
-
+param(
+    $Image = "*"
+)
 
 "16.5","17","17.1" | ForEach-Object {
     docker build -t paper:$_ -f ./base/Dockerfile.$_ ./base
 }
 
-"blockhunt", "clipso-mega-modded", "manhunt", "minigames" | ForEach-Object {
+"blockhunt", "clipso-mega-modded", "manhunt", "minigames", "terminator" | ?{$_ -like $Image} | ForEach-Object {
     docker build -t $_ ./$_
 }
