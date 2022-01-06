033892534aaa5703a8febd76f37b056737a6c3eb
Chris Duck <gpduck@whatsupduck.net>
2022-01-06 00:14:21 -0600

    update velocity to support external servers

--- velocity/startup.ps1
+++ velocity/startup.ps1
@@ -39,11 +39,20 @@ if($env:OP) {
 $mcinfo = Get-Content /etc/mcinfo | ConvertFrom-Json
 $velocityConfig = [io.file]::ReadAllText("/velocity/velocity.toml")
 $ServerList = @()
-for($i = 1; $i -le $mcinfo.lobbyCount; $i++) {
-    $ServerList += " lobby = 'manhunt-lobby-${i}:25565'"
-}
-for($i = 1; $i -le $mcinfo.manhuntCount; $i++) {
-    $ServerList += " manhunt$i = 'manhunt-manhunt-${i}:25565'"
+
+if($mcinfo.proxiedServers) {
+    $mcinfo.proxiedServers | Get-Member -MemberType NoteProperty | ForEach-Object {
+        $Name = $_
+        $Value = $mcinfo.proxiedServers[$Name]
+        $ServerList += " $Name = ${value}:25565"
+    }
+} else {
+    for($i = 1; $i -le $mcinfo.lobbyCount; $i++) {
+        $ServerList += " lobby = 'manhunt-lobby-${i}:25565'"
+    }
+    for($i = 1; $i -le $mcinfo.manhuntCount; $i++) {
+        $ServerList += " manhunt$i = 'manhunt-manhunt-${i}:25565'"
+    }
 }
 $velocityConfig = $velocityConfig.replace("##SERVERS##", @"
 $($ServerList -join "`r`n")
